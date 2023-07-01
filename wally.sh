d26d66520a4ec6f2816010ce95250c2f7cc266de
hahafunnyman <84782396+hahafunnyman@users.noreply.github.com>
2023-07-01 00:29:25 -0600

    update wally.sh

--- wally.sh
+++ wally.sh
@@ -2,7 +2,7 @@
 
 echo Starting Wally Update
 wally install
-rojo sourcemap dev.project.json --output sourcemap.json
+rojo sourcemap default.project.json --output sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages/
 wally-package-types --sourcemap sourcemap.json ServerPackages/
 echo Finishing Wally Update
